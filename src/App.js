7fb2d5f52bc6187fb31aa285f28aa8983155d343
yohannes <yohannes222ethiopia@gmail.com>
2022-10-06 20:16:54 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Container, Input, Spinner, FormFeedback } from 'reactstrap';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [formValue, setformValue] = React.useState({
+		title: '',
+		content: ''
+	});
+
+	const [isLoading, setIsLoading] = React.useState(false);
+	const [isSuccessfull, setIsSuccessfull] = React.useState(false);
+
+	const handleSubmit  = async ()=> {
+		try {
+			setIsLoading(true)
+			const joke = {
+				title: formValue.title,
+				content: formValue.content
+			}
+			const res = await axios.post('https://gabrovowoch-backend.onrender.com/jokes', joke, {
+				headers: {
+					"Access-Control-Allow-Origin": "*",
+					"Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+				}
+			});
+			console.log(res);
+			setIsLoading(false)
+			setIsSuccessfull(true)
+		} catch(err) {
+			console.log(err);
+			setIsLoading(false)
+		}
+	}
+
+	const handleChange = (event) => {
+		setformValue({
+			...formValue,
+			[event.target.name]: event.target.value
+		});
+		setIsSuccessfull(false)
+	}
+
+	return (
+		<Container className="my-5">
+			<p>Title</p>
+			<Input name="title" value={formValue.title} onChange={handleChange} />
+			<p>Content</p>
+			<Input name="content" type="textarea" value={formValue.content} onChange={handleChange} />
+			{isLoading ? <Spinner className='my-3'>Loading...</Spinner> : <Button className="my-3" onClick={handleSubmit}>Post</Button>}
+			{isSuccessfull ? <FormFeedback valid>Sweet! sucessfully added!!</FormFeedback>: " "}
+		</Container>
+	)
 }
 
 export default App;
